20bb708c110ae815799ab76404e552732513448c
Harley Faggetter <topperfalkon@gmail.com>
2013-02-14 00:53:17 +0000

    started work on db install script
    started work on db install script, need to write logic, form, and finish
    declaring variables, but making progress.

---
+++ install.php
@@ -0,0 +1,34 @@
+<?php 
+/*
+* Infinity Corp Manager - install.php
+*
+* By Harley Faggetter
+*
+* Database creation scripts for 
+*/
+
+//declare variables globally TODO: make sure all required variables are declared
+$state = $GET_['state'];
+$dbserver = $POST_['dbserver'];
+$dbuser = $POST_['dbuser'];
+$dbpass = = $POST_['dbpass'];
+
+if ($state == 1)
+{
+	//TODO: Handle user input, create DB, supply response.
+}
+
+else
+{
+	//TODO: Generate form to capture details
+}
+
+
+
+
+
+
+
+
+
+?>
