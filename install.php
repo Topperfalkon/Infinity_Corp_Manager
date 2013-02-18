9a2fd2813dbcdc266d2b7b01fed3eacb006011ef
Harley Faggetter <topperfalkon@gmail.com>
2013-02-18 19:33:55 +0000

    started work on form and db script

--- install.php
+++ install.php
@@ -16,11 +16,37 @@ $dbpass = = $POST_['dbpass'];
 if ($state == 1)
 {
 	//TODO: Handle user input, create DB, supply response.
+	$con = mysql_connect($dbserver,$dbuser,$dbpass);
+	if (!$con)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+	
+	//TODO: Expand the mysql_query to do full create
+	if (mysql_query("CREATE DATABASE my_db",$con))
+	{
+		echo "Database created";
+	}
+	else
+	{
+		echo "Error creating database: " . mysql_error();
+	}
+
+	mysql_close($con);
 }
 
 else
 {
 	//TODO: Generate form to capture details
+	echo
+	("
+		<!-- TODO: Some HTML form here -->
+		<form action=\"/install.php?state=1\" method=\"POST\">
+			Database: <input type=\"text\" name=\"dbserver\">
+			DB Username: <input type=\"password\" name=\"dbuser\">
+			DB Password: <input type=\"password\" name=\"dbpass\">
+		</form> 
+	");
 }
 
 
