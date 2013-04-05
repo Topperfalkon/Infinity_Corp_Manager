07cc3d04ad6e9e57063b83c87f28c809d2c9f97e
Hamish Robertson <hnorobertson@gmail.com>
2013-04-05 19:05:00 +0100

    Changed database tables
     - Renamed 'Corporations' table to 'Groups'
     - Removed bitfield permissions fields - Simplify until we need complex permissions.

--- install.php
+++ install.php
@@ -69,7 +69,7 @@ if (!isset($_POST['submit'])) {
 		$row = $result->fetch_row();
 		if ($row[0]!=='utf8') {
 			die ("It looks like you have created the database <em>$dbname</em>, but you didn't set the character encoding to utf8. Did you know that every time you use something other than utf8, a kitten dies under a pile of quest�on m�rks�<br/>
-	Please change the character encoding to utf8 and the collation to utf8_general_ci (You can use a different collation if you know which non-english language is going to be most commonly used.)");
+	Please change the character encoding to utf8 and the collation to utf8_general_ci/s (You can use a different collation if you know which non-english language is going to be most commonly used.)");
 		} else {
 			echo "Database exists.<br />";
 		}
@@ -83,35 +83,33 @@ if (!isset($_POST['submit'])) {
 			`UserName` VARCHAR(45) NOT NULL,
 			`Password` CHAR(60) NOT NULL,
 			`Email` VARCHAR(80) NOT NULL,
-			`EmailVisible` TINYINT(1) NULL,
-			`EmailVerified` TINYINT(1) NULL,
+			`EmailVisible` BIT(1) NULL,
+			`EmailVerified` BIT(1) NULL,
 			PRIMARY KEY (`UserID`),
 			UNIQUE INDEX `UserName_UNIQUE` (`UserName` ASC) )
-			ENGINE=MyISAM;
+			ENGINE = MyISAM;
 
-		CREATE TABLE IF NOT EXISTS `icmdb`.`Corporations` (
-			`CorporationID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-			`CorpName` VARCHAR(80) NOT NULL,
-			`LeaderID` INT UNSIGNED NOT NULL,
-			`Ticker` VARCHAR(6) NULL DEFAULT NULL,
-			`Description` TEXT NULL,
-			`JoinMode` TINYINT NULL DEFAULT 0 COMMENT '0 = Corp hidden | 1 = Anyone can join | 2 = Anyone can apply, application must be accepted | 3 = Invitation only',
-			`AllowMulti` TINYINT(1) NULL,
-			`HasLogo` TINYINT(1) NULL DEFAULT 0,
-			`Website` VARCHAR(80) NULL,
-			`DefaultPermissions` BINARY(1) NULL COMMENT 'Bitfield',
-			PRIMARY KEY (`CorporationID`),
-			INDEX `LeaderID` (`LeaderID` ASC) )
-			ENGINE=MyISAM;
+		CREATE TABLE IF NOT EXISTS `icmdb`.`Groups` (
+		  `GroupID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		  `GroupName` VARCHAR(80) NOT NULL,
+		  `Ticker` VARCHAR(6) NOT NULL,
+		  `Description` TEXT NULL,
+		  `JoinMode` TINYINT NOT NULL DEFAULT 0 COMMENT '0 = Corp hidden | 1 = Closed | 2 = Invitation Only | 3 = Open to applications | 4 = Anyone can join',
+		  `AllowMulti` TINYINT NOT NULL DEFAULT 1,
+		  `HasLogo` BIT(1) NOT NULL DEFAULT 0,
+		  `Website` VARCHAR(80) NULL,
+		  PRIMARY KEY (`GroupID`),
+		  UNIQUE INDEX `GroupName_UNIQUE` (`GroupName` ASC),
+		  UNIQUE INDEX `Ticker_UNIQUE` (`Ticker` ASC) )
+			ENGINE = MyISAM;
 
 		CREATE TABLE IF NOT EXISTS `icmdb`.`Membership` (
-			`UserID` INT UNSIGNED NOT NULL,
-			`CorporationID` INT UNSIGNED NOT NULL,
-			`Status` TINYINT NULL COMMENT '0 = Applied | 1 = Member | 2 = Invited',
-			`Permissions` BINARY(1) NULL COMMENT 'Bitfield',
-			PRIMARY KEY (`UserID`, `CorporationID`),
-			INDEX `Corporation` (`CorporationID` ASC) )
-			ENGINE=MyISAM;"
+		  `UserID` INT UNSIGNED NOT NULL ,
+		  `GroupID` INT UNSIGNED NOT NULL ,
+		  `Status` TINYINT UNSIGNED NOT NULL COMMENT '1 = Member | 2 = Applied | 3 = Invited | 4 = Leader' ,
+		  PRIMARY KEY (`UserID`, `GroupID`) ,
+		  INDEX `GroupID` (`GroupID` ASC) )
+			ENGINE = MyISAM;"
 	)) && ($mysqli->next_result()) && ($mysqli->next_result())) {
 		echo 'Tables created.';
 	} else {
@@ -133,6 +131,6 @@ if (!isset($_POST['submit'])) {
 		'?>')
 	);
 	
-	echo ('Congratulations on succesfully installing the Infinity Corporation Manager Database. For added security, please delete install.php.<br />Settings have been saved to config.php.');
+	echo ('Congratulations on succesfully installing the Infinity Corporation Manager Database. You may now like to delete install.php just to tidy up.<br />Settings have been saved to config.php.');
 }
 ?>
